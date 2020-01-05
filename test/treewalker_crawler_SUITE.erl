b6cb3337d72680171a7080284dfcc5589647672c
Antoine Gagné <gagnantoine@gmail.com>
2020-01-05 16:18:07 -0500

    Add test file

---
+++ test/treewalker_crawler_SUITE.erl
@@ -0,0 +1,21 @@
+-module(treewalker_crawler_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(nowarn_export_all).
+-compile(export_all).
+
+all() ->
+    [
+    ].
+
+init_per_testcase(_Name, Config) ->
+    Config.
+
+end_per_testcase(_Name, Config) ->
+    Config.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
